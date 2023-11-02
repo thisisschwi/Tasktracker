25c7dacc736529ded73fda0f422083aea0b98718
Kelvin Wong <144655682+thisisschwi@users.noreply.github.com>
2023-11-02 16:57:39 -0400

    index move back and home route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home',
+    heading: 'Home only'
+  });
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home',
   heading: 'Home only'
