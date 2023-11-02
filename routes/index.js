40ad61195576355f10a492133e19e0b851eebaac
Kelvin Wong <144655682+thisisschwi@users.noreply.github.com>
2023-11-02 15:59:49 -0400

    Assignment 2 update

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home',
+  heading: 'Home only'
+  });
+});
+/* GET About page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+/* GET Products page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET Contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+
+module.exports = router;
